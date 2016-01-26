9820d4cf664f48cc7e8619cb33f3a88654efe716
Matt Burke <git@mattburkedev.com>
2016-01-26 11:47:40 -0600

    add existing extension

---
+++ README.md
@@ -0,0 +1,16 @@
+# Workfront Accelerator
+
+Adds some features to Workfront for developers.
+
+## Features
+
+### Show subtasks on story board
+
+Appends the subtasks in the middle of each card in the story board.
+
+
+## Development Links
+
+- https://robots.thoughtbot.com/how-to-make-a-chrome-extension
+- http://stackoverflow.com/questions/3209790/jquery-in-google-chrome-content-script
+- http://stackoverflow.com/questions/2963260/how-do-i-auto-reload-a-chrome-extension-im-developing
---
+++ src/extension/showsubtasks.js
@@ -0,0 +1,77 @@
+'use strict';
+
+(function(){
+    var $ = jQuery;
+
+    function loadSubtasks(iterationId) {
+
+        var $hidden = $("input[name='teams']");
+        var teamId = $hidden.val();
+        console.log(teamId);
+
+        var url = "/attask/api-internal/TASK/search";
+
+       var data = {
+           iterationID: iterationId,
+           personal_Mod:"notnull",
+           teamID: teamId,
+           isAgile:true,
+           fields:"backlogOrder,estimate,color,status,statusEquatesWith,assignments:assignedTo:name,children:statusEquatesWith,children:assignedTo,children:assignments:assignedTo",
+           backlogOrder_Sort:"asc",
+           "$$LIMIT":300
+       }
+
+       //var listTemplate = "<li class='sub-task'> <input type='checkbox' autocomplete='off' value='sub-task' name='check-sub-task' class='check-sub-task' id='check_@ID'>  <label class='name' title='@NAME' for='check_@ID'>  <span></span>   <a href='/task/view?ID=@ID&activeTab=tabs-task-updates'>@NAME</a>   </label>    <a href='#'></a></li> ";
+       var listTemplate = "<li class='sub-task'>    <input type='checkbox' autocomplete='off' value='sub-task' name='check-sub-task' class='check-sub-task' id='check_@ID'>     <label class='name' title='@NAME' for='check_@ID'>      <span></span>       <a href='/task/view?ID=@ID&amp;activeTab=tabs-task-updates'>@NAME</a>   </label> </li>";
+
+       $.get(url, data).success(function(result) {
+           console.log(result);
+
+           result.data.forEach(function(story) {
+               console.log(story.ID);
+
+               var $card = $("div.story[data-objid='" + story.ID + "']");
+
+               var $space = $("<div class='story-breakdown-list' style='padding:25px 8px; max-width: 150px'></div>");
+               $space.appendTo($card);
+
+               var $ul = $("<ul class='sub-task'></ul>");
+
+               story.children.forEach(function(subtask) {
+                   var li = listTemplate.replace(/@ID/g, subtask.ID)
+                                        .replace(/@NAME/g, subtask.name);
+                   var $li = $(li);
+
+                   var $input = $li.find("input");
+
+                   $input.prop("disabled", true);
+                   if (subtask.statusEquatesWith == "CPL") {
+                       $input.prop("checked", true);
+                       $li.find("a").css("color", "gray");
+                   }
+
+
+
+                   $li.appendTo($ul);
+               });
+
+               $ul.appendTo($space);
+           });
+       });
+    }
+
+    function waitForIterationId() {
+        var $h3 = $("h3.position-tl a");
+        if ($h3.length == 0) {
+            setTimeout(waitForIterationId, 250);
+            return;
+        }
+
+        var href = $h3.attr("href");
+        var iterationId=href.match(/[a-f0-9]+$/)[0];
+
+        loadSubtasks(iterationId);
+    }
+
+    waitForIterationId();
+})();
